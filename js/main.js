39dae965be139b97367af6176c63da8d6e1f9c12
fanian <rfanian@gmail.com>
2014-06-30 22:50:37 +0400

    commit

--- js/main.js
+++ js/main.js
@@ -3,6 +3,13 @@
     $(document).ready(function() {
         $(".fancybox").fancybox();
 
+        $(".link").fancybox();
+
+        $('#smart ul li').click(function(){
+            var srcLink = $(this).attr('data-src');
+            $('.gallery .bigImage img').attr('src', srcLink);
+        });
+
 
 
 
